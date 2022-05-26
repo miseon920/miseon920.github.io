05ba1322ae1d5a4f7be75301cde127e9b7811f4a
Kim Mi Seon <34021077+miseon920@users.noreply.github.com>
2022-05-26 17:42:55 +0900

    Add files via upload

--- document/js/position06.js
+++ document/js/position06.js
@@ -2,4 +2,7 @@ $(function () {
     $('.top_banner button').on('click', function () {
         $('.top_banner').toggleClass('active').children('.container').slideToggle();
     });
+    $('.right_banner').on('click', function () {
+        $(this).toggleClass('active');
+    });
 });
