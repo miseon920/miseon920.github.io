1f60b7107b67b684c1ea1bb5adaaec03388a2c79
Kim Mi Seon <34021077+miseon920@users.noreply.github.com>
2022-05-31 17:38:18 +0900

    Add files via upload

---
+++ document/js/slick01.js
@@ -0,0 +1,20 @@
+$(function () {
+    //$('.test_slick').slick();
+    $('.main_visual .main_slider').on('init reInit afterChange', function (event, slick, currentSlide) {
+        //console.log(currentSlide);
+        var total = slick.slideCount;
+        $('.num').text(currentSlide ? currentSlide + 1 + '/' + total : 1 + '/' + total);
+        let cur = $('.slick-current');
+        cur.find('a').removeClass('check');
+        //console.log(cur);
+        cur.addClass('on').siblings().removeClass('on');
+        var hover_a = $('.main_visual .main_slider figure.on .inner a');
+        $(hover_a).hover(function () {
+            cur.find('a').addClass('check');
+        });
+    });
+
+    $('.main_visual .main_slider').slick({
+        dots: true,
+    });
+});
