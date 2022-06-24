10a21afabf9ad2043e76d086202dc0679c3a89a8
miseon <miseon920@naver.com>
2022-06-24 11:36:51 +0900

    20220624

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -2,6 +2,7 @@ $(function () {
   $(".topBanner i").click(function () {
     $(".topBanner").slideUp();
   });
+
   $(".mainSlider").on(
     "init afterChange",
     function (event, slick, currentSlide) {
@@ -16,6 +17,7 @@ $(function () {
       );
     }
   );
+
   $(".mainSlider").slick({
     // autoplay: true,
     dots: true,
@@ -23,11 +25,82 @@ $(function () {
     pauseOnHover: false,
     pauseOnFocus: false,
   });
+
   $(".mainArrow i:first-child").click(function () {
     $(".mainSlider").slick("slickPrev");
   });
   $(".mainArrow i:last-child").click(function () {
     $(".mainSlider").slick("slickNext");
+  });
+
+  jQuery("#utb").YTPlayer({
+    videoURL: "https://youtu.be/lKsmjjyITc0",
+    containment: ".utb", //재생위치
+    autoPlay: true,
+    showControls: false,
+    mute: true,
+    startAt: 0,
+    opacity: 1,
+    playOnlyIfsible: true, //동영상위치에 왔을때 재생시키기 위한것
+  });
+
+  $(".utb .zoom").click(function () {
+    $("#utb").YTPFullscreen();
+    return false;
+  });
+
+  $(".productSlider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+    centerMode: true,
+  });
+
+  $(".productArrows .xi-arrow-left").click(function () {
+    $(".productSlider").slick("slickPrev");
+  });
+
+  $(".productArrows .xi-arrow-right").click(function () {
+    $(".productSlider").slick("slickNext");
+  });
+
+  $(".tab_menu li").on("click", function (event) {
+    event.preventDefault();
+    //console.log($(this), $(this).index(), event.target, event.currentTarget);
+    $(".tab_menu li").removeClass("on");
+    $(this).addClass("on");
+    var idx = $(this).index(); // 0,1,2
+    $(".tab_content>div").removeClass("on");
+    $(".tab_content>div").eq(idx).addClass("on");
+  });
+
+  $(".tab_content .notice li").on("click", function (event) {
+    event.preventDefault();
+    var idx = $(this).index(); // 0,1,2
+    $(".right_content>li").removeClass("on");
+    $(".right_content>li").eq(idx).addClass("on");
+    console.log(ux);
+  });
+
+  $(".toTop").on("click", function () {
+    $("html,body").animate({ scrollTop: 0 }, 200);
+  });
+
+  $(window).on("scroll", function () {
+    var sct = $(window).scrollTop();
+    if (sct > 500) {
+      $(".toTop").fadeIn(300);
+    } else {
+      $(".toTop").fadeOut(1000);
+    }
+  });
+
+  $("#fl").on("change", function () {
+    console.log($(this), $(this).val());
+    var lnk = $(this).val();
+    if (lnk) {
+      window.open(lnk);
+    }
   });
 });
 
