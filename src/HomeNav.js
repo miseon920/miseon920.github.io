15e201e0ded905140bf68d200df5bd377318ea3d
miseon <miseon920@naver.com>
2022-10-18 09:13:25 +0900

    20221018

--- src/HomeNav.js
+++ src/HomeNav.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Project from "./Portfolio";
+import Skill from "./Skill";
 
 const HomeNav = () => {
   return (
     <>
       <Routes>
+        <Route path="/skill" element={<Skill /> }></Route>
         <Route path="/Portfolio/:id" element={<Project />}></Route>
       </Routes>
     </>
---
+++ src/Skill.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { skill } from "./Data";
+const Skill = () => {
+  return (
+    <div className='skill inner sub'>
+        <ul>
+            {
+            skill.map(skill => (
+                <li key={skill.id}>
+                    <div><img src={`${ process.env.PUBLIC_URL }/img/${ skill.name }.png`} alt={skill.name} />{skill.name}</div>
+                    <div>{skill.skill}</div> 
+                </li>
+            ))
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default Skill
