4790e6e1905e77b5686f556ceb06d226db96ced6
miseon920 <miseon920@naver.com>
2024-12-20 17:23:27 +0900

    resume update

--- src/components/RESUME.js
+++ src/components/RESUME.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { skill } from "../data/Data";
-import { FiArrowRight } from "react-icons/fi";
-import { Link } from "react-router-dom";
-const RESUME = ({ content }) => {
+import {skill} from "../data/Data";
+import {FiArrowRight} from "react-icons/fi";
+import {Link} from "react-router-dom";
+const RESUME = ({content}) => {
   const scon = content[1].content;
   //console.log(scon);
   return (
@@ -32,16 +32,11 @@ const RESUME = ({ content }) => {
                         <div>2018.01~2020.02 [서울]</div>
                         <div>
                           <span>업무</span>
-                          <span>
-                            프론트엔드,퍼블리싱,유지보수,까페24,그누보드,영카트,고도몰,부트스트랩,php기반
-                            홈페이지, 하이브리드 앱 퍼블리싱
-                          </span>
+                          <span>프론트엔드,퍼블리싱,유지보수,까페24,그누보드,영카트,고도몰,부트스트랩,php기반 홈페이지, 하이브리드 앱 퍼블리싱</span>
                         </div>
                         <div>
                           <span>Tool</span>
-                          <span>
-                            포토샵,제플린,브라켓,에디트플러스,파일질라
-                          </span>
+                          <span>포토샵,제플린,브라켓,에디트플러스,파일질라</span>
                         </div>
                       </li>
                       <li>
@@ -68,6 +63,30 @@ const RESUME = ({ content }) => {
                         <div>
                           <span>Tool</span>
                           <span>VSCODE,파일질라,그누보드</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>더블미디어(기술개발1팀 / 매니저)</div>
+                        <div>2022.12 ~ 2024.02 [부산]</div>
+                        <div>
+                          <span>업무</span>
+                          <span>vue를 활용한 자사홈페이지 프론트엔드 작업,Typescript 및 RESTful API 연동,Git을 이용하여 버전관리 및 코드 관리, 컴포넌트 유지보수 및 개발,react를 활용한 자사 뉴홈페이지 프론트엔드 작업</span>
+                        </div>
+                        <div>
+                          <span>Tool</span>
+                          <span>포토샵,제플린,VSCODE,GitHub,GitLab,Git fork</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>토브소프트 (개발팀 · 임시직 / 프리랜서)</div>
+                        <div>2024.02 ~ 2024.06 [서울]</div>
+                        <div>
+                          <span>업무</span>
+                          <span>교육용 콘텐츠 개발,미래엔 엠티처 초등 교육용 콘텐츠 개발,아이스쿨 국어 교육용 콘텐츠 개발,자바스크립트 및 제이쿼리를 활용한 퍼블리싱 및 개발</span>
+                        </div>
+                        <div>
+                          <span>Tool</span>
+                          <span>포토샵,일러스트,VSCODE,SVN,파일질라</span>
                         </div>
                       </li>
                     </ul>
@@ -105,10 +124,7 @@ const RESUME = ({ content }) => {
                       {skill.map((skill) => (
                         <li key={skill.id}>
                           <div>
-                            <img
-                              src={`${process.env.PUBLIC_URL}/img/${skill.name}.png`}
-                              alt={skill.name}
-                            />
+                            <img src={`${process.env.PUBLIC_URL}/img/${skill.name}.png`} alt={skill.name} />
                             <span>{skill.name}</span>
                           </div>
                         </li>
